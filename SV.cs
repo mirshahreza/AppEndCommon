19d761f32c3f258b9525ef69d2ac36b6cf13b034
Mohsen Mirshahreza <mirshahreza@outlook.com>
2023-07-04 09:19:05 +0330

    GetMethodBody moved to SV

--- SV.cs
+++ SV.cs
@@ -11,5 +11,31 @@ namespace AppEnd
         public static string NT => "\t";
         public static string NL => Environment.NewLine;
         public static string NL2x => Environment.NewLine + Environment.NewLine;
+
+        public static string CSharpClassBody => @"
+using System;
+using System.Text.Encodings;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using AppEndDbIO;
+
+namespace $Namespace$
+{
+    public static class $ClassName$
+    {
+$Methods$
+    }
+}";
+        
+
+        public static string CSharpMethodBody => @"
+        public static object? $MethodName$(JsonElement ClientQueryJE)
+        {
+            return AppEndDbIO.ClientQuery.Instance(ClientQueryJE).Exec();
+        }";
+        
+
     }
 }
