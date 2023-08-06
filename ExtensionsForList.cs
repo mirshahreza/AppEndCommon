9e63f2e7db1c1821e7595a454778ec2155a8b3f5
Mohsen Mirshahreza <mirshahreza@outlook.com>
2023-08-06 03:56:52 +0330

    AddSafe added to ExtensionsForList

--- ExtensionsForList.cs
+++ ExtensionsForList.cs
@@ -5,13 +5,20 @@ namespace AppEnd
 {
     public static class ExtensionsForList
     {
-        public static void AddIfNotContains(this List<string> list, string name)
-        {
-            if (!list.Contains(name))
-            {
-                list.Add(name);
-            }
-        }
+		public static void AddIfNotContains(this List<string> list, string name)
+		{
+			if (!list.Contains(name))
+			{
+				list.Add(name);
+			}
+		}
+		public static void AddSafe(this List<string> list, string? name)
+		{
+			if (name is not null)
+			{
+				list.Add(name);
+			}
+		}
 
-    }
+	}
 }
