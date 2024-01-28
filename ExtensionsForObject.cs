a4332496bd86967dd206ca1ba19beb1f9edb5b4c
Mohsen Mirshahreza <mirshahreza@outlook.com>
2024-01-28 16:18:49 +0330

    Login and set other parameters implemented

--- ExtensionsForObject.cs
+++ ExtensionsForObject.cs
@@ -7,19 +7,31 @@
             if (o is null) return "";
             return o.ToString();
         }
-        public static int ToIntSafe(this object? o, int ifHasProblem = -1)
-        {
-            string i = o.ToStringEmpty();
-            if (i.IsNullOrEmpty()) return ifHasProblem;
-            int ii;
-            if (int.TryParse(i, out ii))
-            {
-                return ii;
-            }
-            return ifHasProblem;
-        }
+		public static int ToIntSafe(this object? o, int ifHasProblem = -1)
+		{
+			string i = o.ToStringEmpty();
+			if (i.IsNullOrEmpty()) return ifHasProblem;
+			int ii;
+			if (int.TryParse(i, out ii))
+			{
+				return ii;
+			}
+			return ifHasProblem;
+		}
 
-        public static object FixNull(this object? o, object ifNull)
+		public static bool ToBooleanSafe(this object? o, bool ifHasProblem = false)
+		{
+			string i = o.ToStringEmpty();
+			if (i.IsNullOrEmpty()) return ifHasProblem;
+			bool ii;
+			if (bool.TryParse(i, out ii))
+			{
+				return ii;
+			}
+			return ifHasProblem;
+		}
+
+		public static object FixNull(this object? o, object ifNull)
         {
             if (o is null && ifNull is null) throw new ArgumentNullException(nameof(ifNull));
             if (o is null) return ifNull;
