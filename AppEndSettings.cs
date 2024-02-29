b54a485018a454b5c073fde8e7dddc470f5f3a7f
Mohsen Mirshahreza <mirshahreza@outlook.com>
2024-02-29 10:21:21 +0330

    Context methods moved to AppEndCommon Some settings removed

--- AppEndSettings.cs
+++ AppEndSettings.cs
@@ -3,28 +3,30 @@ using System.Text.Json.Nodes;
 
 namespace AppEnd
 {
-	public class AppEndSettings
+	public static class AppEndSettings
     {
-        private static string? serverObjectsPath;
-        private static string? clientObjectsPath;
-        private static JsonArray? dbServers;
+
+		public const string ConfigSectionName = "AppEnd";
+		public static List<string> ReservedFolders = ["..lib", "..templates", "appendstudio", ".DbComponents", ".PublicComponents", ".SharedComponents"];
+
+		private static JsonArray? dbServers;
         public static JsonArray DbServers
         {
             get
             {
                 if (dbServers == null)
                 {
-                    if (AppSettings["AppEnd"] == null) AppSettings["AppEnd"] = JsonNode.Parse("{}")?.AsObject();
-                    if (AppSettings["AppEnd"]?[nameof(DbServers)] == null)
+                    if (AppSettings[ConfigSectionName] == null) AppSettings[ConfigSectionName] = JsonNode.Parse("{}")?.AsObject();
+                    if (AppSettings[ConfigSectionName]?[nameof(DbServers)] == null)
                     {
-						if (AppSettings["AppEnd"] == null)
+						if (AppSettings[ConfigSectionName] == null)
 						{
 							throw new AppEndException("AppSettingsFileMustContains")
 								.AddParam("Section", "AppEnd:ServerObjectsPath")
 								.AddParam("Site", $"{System.Reflection.MethodBase.GetCurrentMethod()?.DeclaringType?.Name}, {System.Reflection.MethodBase.GetCurrentMethod()?.Name}")
 								;
 						}
-						AppSettings["AppEnd"][nameof(DbServers)] = JsonNode.Parse("[]")?.AsArray();
+						AppSettings[ConfigSectionName][nameof(DbServers)] = JsonNode.Parse("[]")?.AsArray();
                         string s = JsonSerializer.Serialize(AppSettings, options: new()
                         {
                             WriteIndented = true
@@ -32,7 +34,7 @@ namespace AppEnd
                         File.WriteAllText("appsettings.json", s);
                         appsettings = null;
                     }
-                    dbServers = AppSettings["AppEnd"]?[nameof(DbServers)]?.AsArray();
+                    dbServers = AppSettings[ConfigSectionName]?[nameof(DbServers)]?.AsArray();
                 }
                 return dbServers;
             }
@@ -41,47 +43,105 @@ namespace AppEnd
         {
             get
             {
-                return AppSettings["AppEnd"]?[nameof(WorkspacePath)]?.AsValue().ToString() ?? "workspace";
+                return "workspace";
             }
         }
-        public static string ServerObjectsPath
+		public static string ServerObjectsPath
+		{
+			get
+			{
+				return $"{WorkspacePath}/server";
+			}
+		}
+		public static string ApiCallsPath
+		{
+			get
+			{
+				return $"{WorkspacePath}/apicalls";
+			}
+		}
+		public static string ClientObjectsPath
         {
             get
             {
-                if (serverObjectsPath == null)
-                {
-                    if (AppSettings["AppEnd"] == null)
-                    {
-                        throw new AppEndException("AppSettingsFileMustContains")
-                            .AddParam("Section", "AppEnd:ServerObjectsPath")
-                            .AddParam("Site", $"{System.Reflection.MethodBase.GetCurrentMethod()?.DeclaringType?.Name}, {System.Reflection.MethodBase.GetCurrentMethod()?.Name}")
-                            ;
-                    }
-                    serverObjectsPath = $"{WorkspacePath}/{AppSettings["AppEnd"]?.AsObject()?[nameof(ServerObjectsPath)]?.ToString()}";
-                }
-                return serverObjectsPath;
-            }
-        }
-        public static string ClientObjectsPath
-        {
-            get
-            {
-                if (clientObjectsPath == null)
-                {
-                    if (AppSettings["AppEnd"] == null)
-                    {
-                        throw new AppEndException("AppSettingsFileMustContains")
-                            .AddParam("Section", "AppEnd:ClientObjectsPath")
-                    .AddParam("Site", $"{System.Reflection.MethodBase.GetCurrentMethod()?.DeclaringType?.Name}, {System.Reflection.MethodBase.GetCurrentMethod()?.Name}")
-                            ;
-                    }
-                    clientObjectsPath = $"{WorkspacePath}/{AppSettings["AppEnd"]?.AsObject()?[nameof(ClientObjectsPath)]?.ToString()}";
-                }
-                return clientObjectsPath;
+                return $"{WorkspacePath}/client";
             }
         }
 
-        private static JsonNode? appsettings;
+		public static string LogDbConfName
+		{
+			get
+			{
+				return AppEndSettings.AppSettings[ConfigSectionName]?[nameof(LogDbConfName)]?.ToString() ?? "DefaultRepo";
+			}
+		}
+		public static int LogWriterQueueCap
+		{
+			get
+			{
+				return AppEndSettings.AppSettings[ConfigSectionName]?[nameof(LogWriterQueueCap)]?.ToIntSafe() ?? 0;
+			}
+		}
+		public static string UsersListMethod
+		{
+			get
+			{
+				return AppEndSettings.AppSettings[ConfigSectionName]?[nameof(UsersListMethod)]?.ToString() ?? "DefaultRepo.AAA_Users.ReadListForLoginUse";
+			}
+		}
+		public static string TalkPoint
+		{
+			get
+			{
+				return AppEndSettings.AppSettings[ConfigSectionName]?[nameof(TalkPoint)]?.ToString() ?? "talk-to-me";
+			}
+		}
+		public static string PublicKeyRole
+		{
+			get
+			{
+				return AppEndSettings.AppSettings[ConfigSectionName]?[nameof(PublicKeyRole)]?.ToString() ?? "";
+			}
+		}
+		public static string DefaultSuccessLoggerMethod
+		{
+			get
+			{
+				return AppEndSettings.AppSettings[ConfigSectionName]?[nameof(DefaultSuccessLoggerMethod)]?.ToString() ?? "";
+			}
+		}
+		public static string DefaultErrorLoggerMethod
+		{
+			get
+			{
+				return AppEndSettings.AppSettings[ConfigSectionName]?[nameof(DefaultErrorLoggerMethod)]?.ToString() ?? "";
+			}
+		}
+		public static string PublicKeyUser
+		{
+			get
+			{
+				return AppEndSettings.AppSettings[ConfigSectionName]?[nameof(PublicKeyUser)]?.ToString() ?? "";
+			}
+		}
+		public static string[]? PublicMethods
+		{
+			get
+			{
+				return AppEndSettings.AppSettings[ConfigSectionName]?[nameof(PublicMethods)]?.ToString().DeserializeAsStringArray();
+			}
+		}
+
+		public static string Secret
+		{
+			get
+			{
+				return AppEndSettings.AppSettings[ConfigSectionName]?[nameof(Secret)]?.ToString() ?? ConfigSectionName;
+			}
+		}
+
+
+		private static JsonNode? appsettings;
         public static JsonNode AppSettings
         {
             get
