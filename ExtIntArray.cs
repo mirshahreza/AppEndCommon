2400e0f3e9525868cba1a9255bedc4eacb203b16
Mohsen Mirshahreza <m_mirshahreza@yahoo.com>
2025-05-05 09:45:40 +0330

    ExtIntArray

---
+++ ExtIntArray.cs
@@ -0,0 +1,16 @@
+﻿namespace AppEndCommon
+{
+	public static class ExtIntArray
+	{
+		public static bool HasIntersect(this int[] arr, int[]? testArr)
+		{
+			if (testArr is null || testArr.Length == 0) return false;
+			foreach (int str in arr)
+			{
+				if (testArr.Contains(str)) return true;
+			}
+			return false;
+		}
+
+	}
+}
