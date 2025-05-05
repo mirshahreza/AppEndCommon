d667a08699b2587d33898ac4fe166ba6524aa406
Mohsen Mirshahreza <m_mirshahreza@yahoo.com>
2025-05-05 09:57:52 +0330

    Bugfix

--- ExtIntArray.cs
+++ ExtIntArray.cs
@@ -2,9 +2,10 @@
 {
 	public static class ExtIntArray
 	{
-		public static bool HasIntersect(this int[] arr, int[]? testArr)
+		public static bool HasIntersect(this int[]? arr, int[]? testArr)
 		{
 			if (testArr is null || testArr.Length == 0) return false;
+			if (arr is null || arr.Length == 0) return false;
 			foreach (int str in arr)
 			{
 				if (testArr.Contains(str)) return true;
