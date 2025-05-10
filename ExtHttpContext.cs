2bfcb7a4a6f7aa89ca33ce946f05c8ae91270d86
Mohsen Mirshahreza <m_mirshahreza@yahoo.com>
2025-05-10 08:19:20 +0330

    IsPostFace added

---
+++ ExtHttpContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppEndCommon
+{
+	public static class ExtHttpContext
+	{
+		public static string GetClientIp(this HttpContext context)
+		{
+			return context.Connection.RemoteIpAddress.MapToIPv4().ToString();
+		}
+		public static string GetClientAgent(this HttpContext context)
+		{
+			return context.Request.Headers["User-Agent"].ToString();
+		}
+		public static bool IsPostFace(this HttpContext context)
+		{
+			return context.Request.Method == HttpMethods.Post || context.Request.Method == HttpMethods.Put || context.Request.Method == HttpMethods.Patch;
+		}
+
+	}
+}
